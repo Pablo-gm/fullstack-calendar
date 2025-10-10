4241ac6145edc74da2089e93603863fd127d45eb
Pablo Gomez <pablogomez321@gmail.com>
2025-10-09 22:08:37 -0600

    beer css

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# fullstack-calendar
+# Fullstack Calendar
+
+## Screnshots
+
+## About the project
+
+## Features
+
+## Tech stack
+
+## Folder structure
+
+## Instructions
+
+https://backpackforlaravel.com/articles/tutorials/quickest-way-to-setup-php-environment-laravel-herd-mysql
+https://blog.logrocket.com/top-6-css-frameworks-2025/
+https://www.youtube.com/watch?v=uq4fn40Oans
---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/resources/css/beer.min.css" />
+    <link rel="stylesheet" href="/resources/css/theme.css" />
+</head>
+<body>
+    Hello World
+    <button>Button</button>
+</body>
+</html>
