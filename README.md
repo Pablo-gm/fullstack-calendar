022639f15272ef83b31a41d7636867ebf0fca893
Pablo Gomez <pablogomez321@gmail.com>
2025-11-13 08:03:24 -0600

    README adjustments

--- README.md
+++ README.md
@@ -1,13 +1,32 @@
 # Fullstack Calendar
 
 ## Screnshots
+![Vista add](resources/preview/calendar.png)
 
 ## About the project
+A calendar and appointment manager built with **PHP**, **MySQL**, **JavaScript**, **HTML**, and **CSS**.
 
 ## Features
+- ✅ Add, edit, and delete appointments
+- ✅ Support for **multi-day** events
+- ✅ Handle **overlapping** bookings
+- ✅ Set **start/end time slots**
+- ✅ Navigate between months
+- ✅ Dropdown controls for managing multiple bookings per day
 
 ## Tech stack
-
-## Folder structure
+- **Frontend**: HTML, CSS ([beercss](https://github.com/beercss/beercss)), JavaScript 
+- **Backend**: PHP (Procedural)
+- **Database**: MySQL
+- **Architecture**: MVC-inspired folder structure
 
 ## Instructions
+Create a database on MySQL
+Create a table using the appointments.sql file
+Create a .env file on root with the following variables:
+
+```c
+DB_HOST= *localhost or any url*
+DB_USER=
+DB_PASSWORD=
+```
---
+++ appointments.sql
@@ -0,0 +1,56 @@
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `fullstack_calendar`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `appointments`
+--
+
+CREATE TABLE `appointments` (
+  `id` int NOT NULL,
+  `course_name` varchar(255) NOT NULL,
+  `instructor_name` varchar(255) NOT NULL,
+  `start_date` date NOT NULL,
+  `end_date` date NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `start_time` time NOT NULL,
+  `end_time` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `appointments`
+--
+ALTER TABLE `appointments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `appointments`
+--
+ALTER TABLE `appointments`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
